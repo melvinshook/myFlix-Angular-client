4d8e8f29ab9312545342c0076efeeff783ef6f71
melvinshook <139077191+melvinshook@users.noreply.github.com>
2024-05-08 14:27:17 -0400

    added user registration and updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ɵPLATFORM_BROWSER_ID } from '@angular/common';
 import bootstrap from '../main.server';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { NgModule } from '@angular/core';
+
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        UserRegistrationFormComponent
     ],
     imports: [
-    
-        HttpClientModule
+        BrowserModule,
+        HttpClientModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        MatDialogModule,
+        MatInputModule,
+        MatButtonModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatSnackBarModule
     ],
     providers: [],
     bootstrap: [AppComponent]
 
 })
 
-export default AppModule {}; 
+export class AppModule { } 
