413b673eb386be17c69b2ac15564c86f4a123a46
melvinshook <139077191+melvinshook@users.noreply.github.com>
2024-05-11 15:42:00 -0400

    added user login

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,16 +1,13 @@
-// src/app/user-registration-form/user-registration-form.component.ts
 import { Component, OnInit, Input } from '@angular/core';
 
 // You'll use this import to close the dialog on success
 import { MatDialogRef } from '@angular/material/dialog';
 
-
+// This import brings in the API calls we created in 6.2
+import { UserRegistrationService } from '../fetch-api-data.service';
 
 // This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
-
-// This import brings in the API from exercise 6.2
-import { UserRegistrationService } from '../fetch-api-data.service';
 
 
 @Component({
@@ -46,3 +43,4 @@ registerUser(): void {
   }
 
   }
+
